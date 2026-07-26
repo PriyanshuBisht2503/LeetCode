98067712aae2e3201a70d1fc6e0a16ad35ec6289
Priyanshu Bisht <138841584+PriyanshuBisht2503@users.noreply.github.com>
2026-07-26 08:06:34 +0530

    Time: 4 ms (37.37%) | Memory: 43.4 MB (41.44%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()==1) return false;
+
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }else{
+                if(st.isEmpty() && (ch==')' || ch=='}' || ch==']')) {
+                    return false;
+                }else{
+                if(ch==')' && st.peek()=='(' || (ch=='}' && st.peek()=='{') || (ch==']' && st.peek()=='[')){
+                    st.pop();
+                }else{
+                    return false;
+                }
+                }
+            }
+        }if(st.isEmpty()){
+            return true;
+        }
+        return false;
+
+    }
+}
